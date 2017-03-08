a16cba9107088ddb5b76c05e0762edc58ecc749b
quan <thanhquan.ng@gmail.com>
2017-03-08 20:38:44 +0700

    M2 - init

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :rooms
+  resources :rooms do
+    resources :messages
+  end
+
   root 'rooms#index'
   get 'logout' => 'home#logout'
   post 'login' => 'home#login'
